260699ddd10e708150f4d2659faf23d39aa847bd
Jin-sung <dev.jinsung1017@gmail.com>
2024-06-15 02:06:23 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ interface IRepo {
     files: {
       [filename]: {
         // eslint-disable-next-line quotes
-        filename: (morning + daytime) > (evening + night) ? "I'm an early 🐤" : "I'm a night 🦉",
+        filename: (morning) > (datatime + evening + night) ? "I'm an early 🐤" : "I'm a night 🦉",
         content: lines.join('\n'),
       },
     },
